703ae61b69986bb07dff17fb929ec88ca6b322ba
renatomrocha <renatomrocha93@gmail.com>
2022-06-08 11:35:21 +0100

    Some suggestions on current state of the Single LP Pool

--- contracts/CosmicLPT.sol
+++ contracts/CosmicLPT.sol
@@ -1,6 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.5.16;
 
+// REVIEW -> Here you can use the Openzeppelin standard, this approach is actually correct since it's an ERC20 nonetheless, it's just that using pre-baked contracts is more convenient
+// and makes it easier to customize your token with battle tested code in a more succinct and error proof way, something like:
+
+// --------------------------------- EXAMPLE ----------------------------------------- //
+
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// contract CosmicLPT is ERC20 {
+
+//     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+
+//  function decimals() public view virtual override returns (uint8) {
+//         return 18;
+//     }
+//     function mint(address _to, uint256 _amount) external returns(bool){
+//         _mint(_to, _amount);
+//         return true;
+//     }
+
+// }
+
+// * You can customize it as you please 
+// REFS:
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
+// https://docs.openzeppelin.com/contracts/4.x/erc20
+
 contract CosmicLPT {
     string  public name = "CosmicLPToken";
     string  public symbol = "CLPT";
